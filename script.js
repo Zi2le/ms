c8b3af67c42e53cb68e8716d34737869bba99d4c
Zi2le <onaopemipoojedokun@gmail.com>
2023-02-23 06:07:12 +0100

    fix

---
+++ script.js
@@ -0,0 +1,8 @@
+let myName= 'onaopemipo'
+myName= `funmilayo`
+age = '22'
+phnoNumber = '08070792265'
+
+let statement = `my name is ${myName} and i am ${age} years old 
+you can contact me on ${phnoNumber}`
+alert(statement)
